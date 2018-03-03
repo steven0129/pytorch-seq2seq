bda4902205abb54f08380030985595e31ebee9f7
steven0129 <h3584935@gmail.com>
2018-03-03 17:06:16 +0800

    Clean code

--- main.py
+++ main.py
@@ -18,25 +18,25 @@ def train(**kwargs):
 
     vis = Visualizer(env=options.env)
     poet = Poet(type='train', ratio=options.ratio)
-    [encoderLengths, decoderLengths] = lmap(lambda x: torch.zeros(len(x)).long(), [poet, poet])
+    [enLengths, deLengths] = lmap(lambda x: torch.zeros(len(x)).long(), [poet, poet])
 
     print('正在計算seq最大長度...')
-    for i, [encoderLength, decoderLength] in tqdm(emap(getSeqLength, poet), total=len(poet)):
+    for i, [enLength, deLength] in tqdm(emap(getSeqLength, poet), total=len(poet)):
         # TODO: 有沒有方法可以加速?
-        [encoderLengths[i], decoderLengths[i]] = lmap(lambda x: x, [encoderLength, decoderLength])
+        [enLengths[i], deLengths[i]] = lmap(lambda x: x, [enLength, deLength])
 
-    [maxEncoderLength, maxDecoderLength] = lmap(torch.max, [encoderLengths, decoderLengths])  # encoder, decoder最大長度
+    [maxEnLength, maxDeLength] = lmap(torch.max, [enLengths, deLengths])  # encoder, decoder最大長度
 
     print('padding sequences...')
-    [encoderPadded, decoderPadded] = lmap(lambda x, y: torch.zeros(len(x), y), [poet, poet],
-                                          [maxEncoderLength, maxDecoderLength])
+    [enPadded, dePadded] = lmap(lambda x, y: torch.zeros(len(x), y), [poet, poet],
+                                          [maxEnLength, maxDeLength])
     for i, [data, result] in tqdm(enumerate(poet), total=len(poet)):
         # TODO: 有沒有方法可以加速?
-        [encoderPadded[i], decoderPadded[i]] = lmap(lambda x, y, z: padSeq(x, y, z), [data, result],
-                                                    [maxEncoderLength, maxDecoderLength], [poet.PAD, poet.PAD])
+        [enPadded[i], dePadded[i]] = lmap(lambda x, y, z: padSeq(x, y, z), [data, result],
+                                                    [maxEnLength, maxDeLength], [poet.PAD, poet.PAD])
 
-    [encoderTensor, decoderTensor] = lmap(torch.stack, [encoderPadded, decoderPadded])
-    dataset = D.TensorDataset(data_tensor=encoderTensor.long(), target_tensor=decoderTensor.long())
+    [enTensor, deTensor] = lmap(torch.stack, [enPadded, dePadded])
+    dataset = D.TensorDataset(data_tensor=enTensor.long(), target_tensor=deTensor.long())
     loader = D.DataLoader(dataset=dataset, batch_size=options.batch_size, num_workers=options.CPU)
 
     encoder = Encoder.RNN(poet.getWordDim(), options.hidden_size, options.encoder_layers, options.dropout)
@@ -53,13 +53,13 @@ def train(**kwargs):
             sortBatch = lambda x, y: zip(*sorted(zip(x, y), key=lambda x: x[0], reverse=True))
             [(lenX, batchX), (lenY, batchY)] = lmap(sortBatch, [lenX, lenY], [batchX, batchY])
 
-            #  存成Variable
+            # 存成Variable
             [varX, varY] = lmap(lambda x: Variable(torch.LongTensor(x)).transpose(0, 1).contiguous(), [batchX, batchY])
             [varX, varY] = lmap(lambda x: x.cuda() if options.use_gpu else x, [varX, varY])
 
             # 輸入encoder
-            encoderOut, encoderHidden = encoder(varX, list(lenX), None)
-            tqdm.write(str(encoderHidden.size()))
+            enOut, enHidden = encoder(varX, list(lenX), None)
+            tqdm.write(str(enHidden.size()))
 
 
 def saveNpz(**kwargs):
