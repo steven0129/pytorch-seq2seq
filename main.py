2dd46cd3169f86077c8da4c46e049a78f3f40925
steven0129 <h3584935@gmail.com>
2018-03-03 15:40:17 +0800

    Clean code

--- main.py
+++ main.py
@@ -23,7 +23,17 @@ def getSeqLength(arr):
     return myMap(arr)
 
 
+def emap(*args):
+    return enumerate(map(*args))
+
+
+def lmap(*args):
+    return list(map(*args))
+
+
 def train(**kwargs):
+    lmap(lambda x, y: x + y, [1], [2])
+
     for k, v in kwargs.items():
         setattr(options, k, v)
 
@@ -34,27 +44,26 @@ def train(**kwargs):
     poet = Poet(type='train', ratio=options.ratio)
 
     # 求seq最大長度
-    [encoderLengths, decoderLengths] = list(map(lambda x: torch.zeros(len(x)).long(), [poet, poet]))
+    [encoderLengths, decoderLengths] = lmap(lambda x: torch.zeros(len(x)).long(), [poet, poet])
 
     print('正在計算seq最大長度...')
-    for i, [encoderLength, decoderLength] in tqdm(enumerate(map(getSeqLength, poet)), total=len(poet)):
+    for i, [encoderLength, decoderLength] in tqdm(emap(getSeqLength, poet), total=len(poet)):
         # TODO: 有沒有方法可以加速?
-        [encoderLengths[i], decoderLengths[i]] = list(map(lambda x: x, [encoderLength, decoderLength]))
+        [encoderLengths[i], decoderLengths[i]] = lmap(lambda x: x, [encoderLength, decoderLength])
 
-    [maxEncoderLength, maxDecoderLength] = list(
-        map(torch.max, [encoderLengths, decoderLengths]))  # encoder, decoder最大長度
+    [maxEncoderLength, maxDecoderLength] = lmap(torch.max, [encoderLengths, decoderLengths])  # encoder, decoder最大長度
 
     # 幫sequence補0
     print('padding sequences...')
-    [encoderPadded, decoderPadded] = list(
-        map(lambda x, y: torch.zeros(len(x), y), [poet, poet], [maxEncoderLength, maxDecoderLength]))
+    [encoderPadded, decoderPadded] = lmap(lambda x, y: torch.zeros(len(x), y), [poet, poet],
+                                          [maxEncoderLength, maxDecoderLength])
     for i, [data, result] in tqdm(enumerate(poet), total=len(poet)):
         # TODO: 有沒有方法可以加速?
-        [encoderPadded[i], decoderPadded[i]] = list(map(lambda x, y, z: padSeq(x, y, z), [data, result],
-                                                        [maxEncoderLength, maxDecoderLength], [poet.PAD, poet.PAD]))
+        [encoderPadded[i], decoderPadded[i]] = lmap(lambda x, y, z: padSeq(x, y, z), [data, result],
+                                                    [maxEncoderLength, maxDecoderLength], [poet.PAD, poet.PAD])
 
     # 訓練Seq2seq Model
-    [encoderTensor, decoderTensor] = list(map(torch.stack, [encoderPadded, decoderPadded]))
+    [encoderTensor, decoderTensor] = lmap(torch.stack, [encoderPadded, decoderPadded])
     dataset = D.TensorDataset(data_tensor=encoderTensor.long(), target_tensor=decoderTensor.long())
     loader = D.DataLoader(dataset=dataset, batch_size=options.batch_size, num_workers=options.CPU)
 
@@ -66,17 +75,16 @@ def train(**kwargs):
     for epoch in tqdm(range(options.epochs)):
         for batchX, batchY in tqdm(loader):
             # 找出batch中每個sequence的長度
-            [batchX, batchY] = list(map(lambda x: x.tolist(), [batchX, batchY]))
-            [lenX, lenY] = list(map(lambda x, y: [s.index(x) + 1 for s in y], [poet.EOS, poet.EOS], [batchX, batchY]))
+            [batchX, batchY] = lmap(lambda x: x.tolist(), [batchX, batchY])
+            [lenX, lenY] = lmap(lambda x, y: [s.index(x) + 1 for s in y], [poet.EOS, poet.EOS], [batchX, batchY])
 
             # 將batch中sequences按長到短進行排序
             sortBatch = lambda x, y: zip(*sorted(zip(x, y), key=lambda x: x[0], reverse=True))
-            [(lenX, batchX), (lenY, batchY)] = list(map(sortBatch, [lenX, lenY], [batchX, batchY]))
+            [(lenX, batchX), (lenY, batchY)] = lmap(sortBatch, [lenX, lenY], [batchX, batchY])
 
             #  存成Variable
-            [varX, varY] = list(
-                map(lambda x: Variable(torch.LongTensor(x)).transpose(0, 1).contiguous(), [batchX, batchY]))
-            [varX, varY] = list(map(lambda x: x.cuda() if options.use_gpu else x, [varX, varY]))
+            [varX, varY] = lmap(lambda x: Variable(torch.LongTensor(x)).transpose(0, 1).contiguous(), [batchX, batchY])
+            [varX, varY] = lmap(lambda x: x.cuda() if options.use_gpu else x, [varX, varY])
 
             # 輸入encoder
             encoderOut, encoderHidden = encoder(varX, list(lenX), None)
