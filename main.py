755bc60541885bbf82dd47a99502256f3744125a
steven0129 <h3584935@gmail.com>
2018-03-03 15:17:30 +0800

    Clean code

--- main.py
+++ main.py
@@ -34,33 +34,28 @@ def train(**kwargs):
     poet = Poet(type='train', ratio=options.ratio)
 
     # 求seq最大長度
-    encoderLengths = torch.zeros(len(poet)).type(torch.LongTensor)
-    decoderLengths = torch.zeros(len(poet)).type(torch.LongTensor)
+    [encoderLengths, decoderLengths] = list(map(lambda x: torch.zeros(len(x)).long(), [poet, poet]))
 
     print('正在計算seq最大長度...')
     for i, [encoderLength, decoderLength] in tqdm(enumerate(map(getSeqLength, poet)), total=len(poet)):
         # TODO: 有沒有方法可以加速?
-        encoderLengths[i] = encoderLength
-        decoderLengths[i] = decoderLength
+        [encoderLengths[i], decoderLengths[i]] = list(map(lambda x: x, [encoderLength, decoderLength]))
 
-    maxDecoderLength = torch.max(decoderLengths)  # decoder最大長度
-    maxEncoderLength = torch.max(encoderLengths)  # encoder最大長度
+    [maxEncoderLength, maxDecoderLength] = list(
+        map(torch.max, [encoderLengths, decoderLengths]))  # encoder, decoder最大長度
 
     # 幫sequence補0
-    encoderPadded = torch.zeros(len(poet), maxEncoderLength)
-    decoderPadded = torch.zeros(len(poet), maxDecoderLength)
-
     print('padding sequences...')
+    [encoderPadded, decoderPadded] = list(
+        map(lambda x, y: torch.zeros(len(x), y), [poet, poet], [maxEncoderLength, maxDecoderLength]))
     for i, [data, result] in tqdm(enumerate(poet), total=len(poet)):
         # TODO: 有沒有方法可以加速?
-        encoderPadded[i] = padSeq(data, maxEncoderLength, poet.PAD)
-        decoderPadded[i] = padSeq(result, maxDecoderLength, poet.PAD)
+        [encoderPadded[i], decoderPadded[i]] = list(map(lambda x, y, z: padSeq(x, y, z), [data, result],
+                                                        [maxEncoderLength, maxDecoderLength], [poet.PAD, poet.PAD]))
 
     # TODO: 訓練Seq2seq Model
-    encoderTensor = torch.stack(encoderPadded)
-    decoderTensor = torch.stack(decoderPadded)
-    dataset = D.TensorDataset(data_tensor=encoderTensor.type(torch.LongTensor),
-                              target_tensor=decoderTensor.type(torch.LongTensor))
+    [encoderTensor, decoderTensor] = list(map(torch.stack, [encoderPadded, decoderPadded]))
+    dataset = D.TensorDataset(data_tensor=encoderTensor.long(), target_tensor=decoderTensor.long())
     loader = D.DataLoader(dataset=dataset, batch_size=options.batch_size, num_workers=options.CPU)
 
     # Encoder with GRU
@@ -71,25 +66,20 @@ def train(**kwargs):
     for epoch in tqdm(range(options.epochs)):
         for batchX, batchY in tqdm(loader):
             # 從長到短排序
-            batchX = batchX.tolist()
-            batchY = batchY.tolist()
-            lenX = [s.index(poet.EOS) + 1 for s in batchX]
-            lenY = [s.index(poet.EOS) + 1 for s in batchY]
+            [batchX, batchY] = list(map(lambda x: x.tolist(), [batchX, batchY]))
+            [lenX, lenY] = list(map(lambda x, y: [s.index(x) + 1 for s in y], [poet.EOS, poet.EOS], [batchX, batchY]))
 
-            pairMap = lambda len, batch: sorted(zip(len, batch), key=lambda x: x[0], reverse=True)
-            pairsX = pairMap(lenX, batchX)
-            pairsY = pairMap(lenY, batchY)
-            lenX, batchX = zip(*pairsX)
-            lenY, batchY = zip(*pairsY)
+            sortBatch = lambda x, y: zip(*sorted(zip(x, y), key=lambda x: x[0], reverse=True))
+            [(lenX, batchX), (lenY, batchY)] = list(map(sortBatch, [lenX, lenY], [batchX, batchY]))
+
+            #  存成Variable
+            [varX, varY] = list(
+                map(lambda x: Variable(torch.LongTensor(x)).transpose(0, 1).contiguous(), [batchX, batchY]))
+            [varX, varY] = list(map(lambda x: x.cuda() if options.use_gpu else x, [varX, varY]))
 
             # 輸入encoder
-            varX = Variable(torch.LongTensor(batchX)).transpose(0, 1).contiguous()
-            varY = Variable(torch.LongTensor(batchY)).transpose(0, 1).contiguous()
-
-            varX = varX.cuda() if options.use_gpu else varX
-            varY = varY.cuda() if options.use_gpu else varY
-
             encoderOut, encoderHidden = encoder(varX, list(lenX), None)
+            tqdm.write(str(encoderHidden.size()))
 
 
 def saveNpz(**kwargs):
