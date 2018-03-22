b1b8ab43c49b7e6b47a0524392ca75f446b467fa
steven0129 <h3584935@gmail.com>
2018-03-22 13:50:23 +0800

    Fix some details

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Env(object):
     use_teacher_forcing = True  # 是否使用Teacher Forcing
     ratio = 0.6  # training data比例
     batch_size = 10  # 批次訓練數量
-    epochs = 100  # 迭帶次數
+    epochs = 100  # 迭代次數
     hidden_size = 5  # hidden unit數量
     encoder_layers = 2  # encoder層數
     dropout = 0.0
